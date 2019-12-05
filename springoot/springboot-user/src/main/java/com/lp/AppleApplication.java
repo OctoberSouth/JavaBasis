0d6d11569f243dbc8117bd8027943aa1fd632c4d
刘攀 <lp123092@163.com>
2019-12-05 22:02:45 +0800

    初始化

---
+++ springoot/springboot-user/src/main/java/com/lp/AppleApplication.java
@@ -0,0 +1,26 @@
+package com.lp;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动
+ * @author 刘攀
+ * @version 1.0
+ * @date 2019/12/5 21:15
+ * @return
+ * @describe
+ */
+@SpringBootApplication
+public class AppleApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) {
+		System.out.println("服务启动完毕.");
+	}
+}
