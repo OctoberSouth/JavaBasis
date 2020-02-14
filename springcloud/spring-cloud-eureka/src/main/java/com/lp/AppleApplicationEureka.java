8334729f202890799a5489d01bca413ee985168f
刘攀 <lp123092@163.com>
2020-02-14 17:23:05 +0800

    eureka

---
+++ springcloud/spring-cloud-eureka/src/main/java/com/lp/AppleApplicationEureka.java
@@ -0,0 +1,28 @@
+package com.lp;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册服务
+ * 
+ * @author 刘攀
+ * @time 2019年11月8日下午2:47:53
+ * @version 1.0
+ * @describe
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class AppleApplicationEureka implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleApplicationEureka.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		System.out.println("服务者启动完毕.");
+	}
+}
