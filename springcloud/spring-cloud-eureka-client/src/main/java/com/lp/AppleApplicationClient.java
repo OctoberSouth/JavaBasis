29ae486687dc36ad0dcc51963ecf558decf77a6c
刘攀 <lp123092@163.com>
2020-03-10 10:25:02 +0800

    eureka服务生产者

---
+++ springcloud/spring-cloud-eureka-client/src/main/java/com/lp/AppleApplicationClient.java
@@ -0,0 +1,28 @@
+package com.lp;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 生产者服务
+ * 
+ * @author 刘攀
+ * @time 2019年11月8日下午2:47:53
+ * @version 1.0
+ * @describe
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class AppleApplicationClient implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleApplicationClient.class, args);
+	}
+
+	@Override
+	public void run(String... strings) {
+		System.out.println("服务生产者者启动完毕.");
+	}
+}
