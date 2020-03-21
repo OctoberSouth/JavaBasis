85317dc4f33beb808e3ba3e05151c6850781b86a
刘攀 <lp123092@163.com>
2020-03-21 23:16:13 +0800

    springcloud zuul

--- springcloud/spring-cloud-zuul/src/main/java/com/lp/AppleApplicationZuul.java
+++ springcloud/spring-cloud-zuul/src/main/java/com/lp/AppleApplicationZuul.java
@@ -3,26 +3,26 @@ package com.lp;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
- * 生产者服务
+ * 服务网关
  * 
  * @author 刘攀
  * @time 2019年11月8日下午2:47:53
  * @version 1.0
  * @describe
  */
-@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
-public class AppleApplicationClient implements CommandLineRunner {
+public class AppleApplicationZuul implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppleApplicationClient.class, args);
+		SpringApplication.run(AppleApplicationZuul.class, args);
 	}
 
 	@Override
 	public void run(String... strings) {
-		System.out.println("服务生产者启动完毕.");
+		System.out.println("服务网关启动完毕.");
 	}
 }
