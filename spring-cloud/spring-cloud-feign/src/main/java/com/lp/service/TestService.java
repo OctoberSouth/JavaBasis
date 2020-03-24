1629cba9d40b16254fbd5b9fad7dadcfc76de1a3
刘攀 <lp123092@163.com>
2020-03-24 18:27:49 +0800

    hystrix

--- spring-cloud/spring-cloud-feign/src/main/java/com/lp/service/TestService.java
+++ spring-cloud/spring-cloud-feign/src/main/java/com/lp/service/TestService.java
@@ -1,17 +1,15 @@
 package com.lp.service;
 
 import com.lp.common.ResultJson;
+import com.lp.service.impl.TestServiceFailBackImpl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 微服务调用接口
  */
-@RequestMapping("service")
-//这里为服务的名字
-@FeignClient("lp-service")
+@FeignClient(value = "lp-service", fallback = TestServiceFailBackImpl.class, path = "service")
 public interface TestService {
 
     @PostMapping("test")
