8afa6b5123a32317da5be456ef76b2c484ff5268
刘攀 <lp123092@163.com>
2020-03-25 11:24:22 +0800

    配置简化

--- spring-cloud/spring-cloud-feign/src/main/java/com/lp/AppleApplicationFeign.java
+++ spring-cloud/spring-cloud-feign/src/main/java/com/lp/AppleApplicationFeign.java
@@ -3,8 +3,6 @@ package com.lp;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
